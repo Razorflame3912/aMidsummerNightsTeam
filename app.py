7fde1fe849b0dd268361ceef6fb7cbfcdbb7a58f
michelamar <mmarchini@stuy.edu>
2018-04-12 14:50:31 -0400

    worked a bit

--- app.py
+++ app.py
@@ -12,8 +12,14 @@ full_text = text_file.read()
 text_file.close()
 
 plays_list = full_text.split('the_end')
-#print len(plays_list)
 
+titles = ["All's Well That Ends Well", "Antony and Cleopatra", "As You Like It", "The Comedy of Errors", "Coriolanus", "Cymbeline", "Hamlet", "Henry IV, Part 1", "Henry IV, Part 2", "Henry V", "Henry VI, Part 1", "Henry VI, Part 2", "Henry VI, Part 3", "Henry VIII", "King John", "Julius Caesar", "King Lear", "Love's Labour's Lost", "Macbeth", "Measure For Measure", "The Merchant of Venice", "The Merry Wives of Windsor", "A Midsummer Night's Dream", "Much Ado About Nothing", "Othello", "Richard II", "Richard III", "Romeo and Juliet", "The Taming of the Shrew", "The Tempest", "Timon of Athens", "Titus Andronicus", "Troilus and Cressida", "Twelfth Night", "The Two Gentlemen of Verona", "The Winter's Tale"]
 
+words_dict = {}
 
+for title in titles:
+    words_dict[title] = plays_list[titles.index(title)]
+
+print words_dict["Hamlet"]
+    
 #----------------------------------------------------------------
