09788e35f9787f76b31dafaf778259036efdf943
Mansour <melsharawy@stuy.edu>
2018-04-13 15:24:52 -0400

    adjusted for 0 values in d3

--- templates/script.js
+++ templates/script.js
@@ -17,16 +17,21 @@ var numtopercent = function(list){
     for(i in list){
 	total += list[i];
     }
+    if(total == 0){
+	var newlist = [];
+	for(var i = 0;i<list.length;i++){
+	    newlist.push(-1);
+	}
+	return newlist;
+    }
     for(i in list){
 	list[i] = list[i]/total;
     }
+    //console.log(list);
     return list;
 }
 
 var calc = function(d,arg,mult,current,bigr,offx,offy){
-    if(d==0){
-	d=0.0001;
-    }
     var x1, y1, x2, y2, x3, y3, M1x, M1y, M2x, M2y, m1, m2, cx,cy;
     x1 = (bigr*mult*Math.cos(current*Math.PI*2) + offx);
     y1 = (bigr*mult*Math.sin(current*Math.PI*2) + offy);
@@ -34,13 +39,13 @@ var calc = function(d,arg,mult,current,bigr,offx,offy){
     y2 = (bigr*Math.sin((current + (d/2))*Math.PI*2) + offy);
     x3 = (bigr*mult*Math.cos((current+d)*Math.PI*2) + offx);
     y3 = (bigr*mult*Math.sin((current+d)*Math.PI*2) + offy);
-    /*console.log(x1);
+    /*//console.log(x1);
     console.log(y1);
     console.log(x2);
     console.log(y2);
     console.log(x3);
     console.log(y3);*/
-      /*var line1 = document.createElementNS("http://www.w3.org/2000/svg","line");
+/*      var line1 = document.createElementNS("http://www.w3.org/2000/svg","line");
 		  line1.setAttribute("x1",x1);
 		  line1.setAttribute("y1",y1);
 		  line1.setAttribute("x2",x2);
@@ -76,18 +81,18 @@ var calc = function(d,arg,mult,current,bigr,offx,offy){
     cx = (M1y - M2y + (m2*M2x) - (m1*M1x))/(m2-m1);
     cy = M1y + (m1*(cx-M1x));
     r = Math.sqrt(((cx-x1)*(cx-x1)) + ((cy-y1)*(cy-y1)));
-    console.log(x1);
-    console.log(y1);
-    console.log(x2);
-    console.log(y2);
-    console.log(x3);
-    console.log(y3);
-    console.log(M1x);
-    console.log(M1y);
-    console.log(M2x);
-    console.log(M2y);
-    console.log(m1);
-    console.log(m2);
+    //console.log(x1);
+    //console.log(y1);
+    //console.log(x2);
+    //console.log(y2);
+    //console.log(x3);
+    //console.log(y3);
+    //console.log(M1x);
+    //console.log(M1y);
+    //console.log(M2x);
+    //console.log(M2y);
+    //console.log(m1);
+    //console.log(m2);
     if(arg=="cx"){
 	//console.log(cx);
 	return cx;
@@ -131,7 +136,7 @@ var update = function(e){
 	tragedydata = numtopercent(sizetragedy);
 	historydata = numtopercent(sizehistory);
     }
-    console.log(data);
+    //console.log(data);
     var circles = d3.select("#svg").selectAll("circle.category");
 
     circles.data(data)
@@ -139,6 +144,14 @@ var update = function(e){
 	.duration(2000)
 	.attr("cx",
 	      function(d){
+
+		  if(d==0){
+		      d=0.0001;
+		  }
+		  else if(d<0){
+		      d = (1/3);		      
+		      //   console.log("VALUE OF D: " + d);
+		  }
 		  var ret = calc(d,"cx",mult,current1,bigr,bigr,bigr);
 		  current1 += d;
 		  listcx[icx]=ret;
@@ -147,6 +160,14 @@ var update = function(e){
 	      })
 	.attr("cy",
 	      function(d){
+		  if(d==0){
+		      d=0.0001;
+		  }
+		  else if(d<0){
+		      d = (1/3);
+		      
+		      //   console.log("VALUE OF D: " + d);
+		  }
 		  var ret = calc(d,"cy",mult,current2,bigr,bigr,bigr);
 		  current2 += d;
 		  listcy[icy]=ret;
@@ -157,6 +178,14 @@ var update = function(e){
 	     )
 	.attr("r",
 	      function(d){
+		  if(d==0){
+		      d=0.0001;
+		  }
+		  else if(d<0){
+		      d = (1/3);
+		      
+		   //   console.log("VALUE OF D: " + d);
+		  }
 		  var ret = calc(d,"r",mult,current3,bigr,bigr,bigr);
 		  current3 += d;
 		  listr[ir]=ret;
@@ -181,23 +210,51 @@ var subupdate = function(type,data,offx,offy,bigr){
     var offy = parseFloat(parentcircle.getAttribute("cy"));
     var bigr = parseFloat(parentcircle.getAttribute("r"));*/
     var circles = d3.select("#svg").selectAll("circle."+type);
-    console.log(offx);
-    console.log(offy);
-    console.log(bigr);
-    console.log(circles);
-    console.log(data);
+    //console.log(offx);
+    //console.log(offy);
+    //console.log(bigr);
+    //console.log(circles);
+    //console.log(data);
     
     circles.data(data)
 	.transition()
 	.duration(2000)
 	.attr("cx",
 	      function(d){
+		  if(d==0){
+		      d=0.0001;
+		  }
+		  else if(d<0){
+		      var denominator;
+		      if(type=="comedy"){
+			  denominator = 17;
+		      }
+		      else{
+			  denominator = 10;
+		      }
+		      d = (1/denominator);		      
+		      //   console.log("VALUE OF D: " + d);
+		  }
 		  var ret = calc(d,"cx",mult,current1,bigr,offx,offy);
 		  current1 += d;
 		  return ret;
 	      })
 	.attr("cy",
 	      function(d){
+		  if(d==0){
+		      d=0.0001;
+		  }
+		  else if(d<0){
+		      var denominator;
+		      if(type=="comedy"){
+			  denominator = 17;
+		      }
+		      else{
+			  denominator = 10;
+		      }
+		      d = (1/denominator);		      
+		      //   console.log("VALUE OF D: " + d);
+		  }
 		  var ret = calc(d,"cy",mult,current2,bigr,offx,offy);
 		  current2 += d;
 		  return ret;
@@ -205,6 +262,20 @@ var subupdate = function(type,data,offx,offy,bigr){
 	     )
 	.attr("r",
 	      function(d){
+		  if(d==0){
+		      d=0.0001;
+		  }
+		  else if(d<0){
+		      var denominator;
+		      if(type=="comedy"){
+			  denominator = 17;
+		      }
+		      else{
+			  denominator = 10;
+		      }
+		      d = (1/denominator);		      
+		      //   console.log("VALUE OF D: " + d);
+		  }
 		  var ret = calc(d,"r",mult,current3,bigr,offx,offy);
 		  current3 += d;
 		  return ret;
