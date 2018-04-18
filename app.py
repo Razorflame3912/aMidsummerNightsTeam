16e7dae0ab60f501020a81dc392490a859b78dba
Giorgio Vidali <gvidali@stuy.edu>
2018-04-18 14:46:10 -0400

    working on thinghs

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ plays_list = full_text.split('the_end')
 
 titles = ["All's Well That Ends Well", "Antony and Cleopatra", "As You Like It", "The Comedy of Errors", "Coriolanus", "Cymbeline", "Hamlet", "Henry IV, Part 1", "Henry IV, Part 2", "Henry V", "Henry VI, Part 1", "Henry VI, Part 2", "Henry VI, Part 3", "Henry VIII", "King John", "Julius Caesar", "King Lear", "Love's Labour's Lost", "Macbeth", "Measure For Measure", "The Merchant of Venice", "The Merry Wives of Windsor", "A Midsummer Night's Dream", "Much Ado About Nothing", "Othello", "Richard II", "Richard III", "Romeo and Juliet", "The Taming of the Shrew", "The Tempest", "Timon of Athens", "Titus Andronicus", "Troilus and Cressida", "Twelfth Night", "The Two Gentlemen of Verona", "The Winter's Tale"]
 
+comedies = ["All's Well That Ends Well", "As You Like It", "The Comedy of Errors", "Love's Labour's Lost", "Measure For Measure", "The Merchant of Venice", "The Merry Wives of Windsor", "A Midsummer Night's Dream", "Much Ado About Nothing","The Taming of the Shrew", "The Tempest", "Twelfth Night", "The Two Gentlemen of Verona", "The Winter's Tale"]
+
+tragedies = ["Antony and Cleopatra", "Coriolanus", "Cymbeline"]
+
 words_dict = {}
 
 for title in titles:
